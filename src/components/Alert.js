a70f4817ddb46b37f76924f7c1ee441d0cf025bd
vertuszus <vertuszus@gmail.com>
2021-01-18 22:46:48 +0300

    изменили уведомление при ошибке

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -17,7 +17,7 @@ export const Alert = () => {
             unmountOnExit
         >
             <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
-            <strong>Внимание!</strong> 
+            <strong>Внимание, осторожно!</strong> 
             &nbsp;{alert.text}
             <button onClick={hide} type="button" className="close" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
